05954dbf5b0053ea4fa165d9c94a81e97abb7cbb
denislav-d <ddenislav333@gmail.com>
2025-12-18 15:42:18 +0100

    Add Footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import Image from "next/image";
+
+export default function Footer() {
+    return (
+        <footer className="h-dvh bg-black flex items-center justify-center">
+            <Image src="/DEPT-logo.svg" alt="DEPT® Logo" width={110} height={110} className="invert" />
+        </footer>
+    );
+}
