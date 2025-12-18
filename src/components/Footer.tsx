d7e9ab6010558a5c62269c216007c9c6a6218c5d
denislav-d <ddenislav333@gmail.com>
2025-12-18 18:47:56 +0100

    Update sections and footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Footer() {
     return (
-        <footer className="h-dvh sm:h-[40vh] bg-black flex items-center justify-center">
+        <footer className="h-dvh sm:h-[80vh] bg-black flex items-center justify-center">
             <Image src="/DEPT-logo.svg" alt="DEPT® Logo" width={110} height={110} className="invert" />
         </footer>
     );
--- src/components/ForecastSection.tsx
+++ src/components/ForecastSection.tsx
@@ -1,6 +1,6 @@
 export default function ForecastSection() {
     return (
-        <section className="col-span-full md:col-span-6 py-5 gap-y-17.5 px-5 flex flex-col bg-[#F2F2F2] text-black pl-32 2xl:pl-60 row-span-1">
+        <section className="col-span-full md:col-span-6 py-5 gap-y-17.5 px-5 flex flex-col bg-[#F2F2F2] text-black md:pl-8 lg:pl-16 xl:pl-32 row-span-1">
             Forecast section
         </section>
     );
